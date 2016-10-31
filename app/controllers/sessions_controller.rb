a1b406a8b7fcbb4cf75565cc38849a53571dcc29
markerby <marinakerby@gmail.com>
2016-10-31 23:11:45 +1100

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Sign the user in and redirect to the user's show page.
       sign_in user
-      redirect_to user
-      #render text: 'valid combo'
+      #redirect_to user <- old code - automatically redirects to user's show page
+      redirect_back_or user 
+      #new code - friendly forwarding to the user's location at which they logged in
+      #if no location, redirect to user's show page
     else
       flash.now[:error] = 'Invalid email/password combination' # Not quite right!
       render 'new'
@@ -22,5 +23,18 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_url
   end
+  
+  #redirect_back_or and store_location are for friendly forwarding 
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to) 
+    #delete the forwarding url otherwise the user would be redirected to that URL upon
+    #every signin until they closed their browser 
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+  
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
+  
   def new
   	@user = User.new
   end
@@ -17,4 +22,52 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    #@user = User.find(params[:id]) 
+    #don't have to call the line above now because it is called in correct_user which is called before edit
+  end
+  
+  def update
+    #@user = User.find(params[:id])
+    #don't have to call the line above now because it is called in correct_user which is called before update
+    
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+	  sign_in @user #have to do this because the remember_token is reset on save whihc invalidates the session so have to sign them in again
+	  redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def index
+  	#@users = User.all
+  	@users = User.paginate(page: params[:page]) #pulls out the users one chunk at a time
+  end
+  
+ def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+  
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location #for friendly forwarding to their current location once they are signed in
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+    	redirect_to(root_url) unless current_user.admin?
+    end
 end
